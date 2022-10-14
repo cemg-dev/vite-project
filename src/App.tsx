eb5883139f2d3ba1eed28074c90825fc37dda7ed
Carlos Eduardo de Moura Gomes <cemg@pop-os.localdomain>
2022-10-14 13:59:17 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./style/global.css"
+
+export function App() {
+  
+  return (
+    <>
+        <h1 className="text-bold text-2xl text-center">Hello World</h1>
+        <button className="bg-red-900 text-white rounded px-4 py-2 hover:bg-green-500">Enviar</button>
+    </>
+  );
+
+}
